982123f8b40d0a510f4cfa8ca4e2a008cc7f7240
Alan Ryan <alan@codecrunchers.ie>
2021-01-31 23:13:41 +0000

    working graph

--- src/components/example.rs
+++ src/components/example.rs
@@ -24,25 +24,91 @@ pub fn call_vega() {
 }
 
 pub fn gen_chart() -> Result<Vegalite, Box<dyn std::error::Error>> {
-    // the chart
-    let chart = VegaliteBuilder::default()
-        .title("Stock price")
-        .description("Google's stock price over time.")
-        .data(UrlDataBuilder::default().url(
-            "https://raw.githubusercontent.com/davidB/vega_lite_3.rs/master/examples/res/data/stocks.csv"
-        ).build()?)
-        .transform(vec![
-            TransformBuilder::default().filter("datum.symbol==='GOOG'")
-        .build()?])
-        .mark(Mark::Line)
-        .encoding(EncodingBuilder::default()
-            .x(XClassBuilder::default()
-                .field("date")
-                .def_type(StandardType::Temporal)
-                .build()?)
-            .y(YClassBuilder::default()
-                .field("price")
-                .def_type(StandardType::Quantitative)
-                .build()?).build()?).build()?;
+    let spec = r##"
+    "$schema": "https://vega.github.io/schema/vega/v5.json",
+  "width": 200,
+  "height": 100,
+  "padding": 5,
+
+  "signals": [
+    { "name": "method", "value": "tidy",
+      "bind": {"input": "select", "options": ["tidy", "cluster"]} },
+    { "name": "separation", "value": true, "bind": {"input": "checkbox"} }
+  ],
+
+  "data": [
+    {
+      "name": "tree",
+      "values": [
+        {"id": "A", "parent": null},
+        {"id": "B", "parent": "A"},
+        {"id": "C", "parent": "A"},
+        {"id": "D", "parent": "C"},
+        {"id": "E", "parent": "C"}
+      ],
+      "transform": [
+        {
+          "type": "stratify",
+          "key": "id",
+          "parentKey": "parent"
+        },
+        {
+          "type": "tree",
+          "method": {"signal": "method"},
+          "separation": {"signal": "separation"},
+          "size": [{"signal": "width"}, {"signal": "height"}]
+        }
+      ]
+    },
+    {
+      "name": "links",
+      "source": "tree",
+      "transform": [
+        { "type": "treelinks" },
+        { "type": "linkpath" }
+      ]
+    }
+  ],
+
+  "scales": [
+    {
+      "name": "color",
+      "type": "ordinal",
+      "range": {"scheme": "category20"}
+    }
+  ],
+
+  "marks": [
+    {
+      "type": "path",
+      "from": {"data": "links"},
+      "encode": {
+        "enter": {
+          "stroke": {"value": "#ccc"}
+        },
+        "update": {
+          "path": {"field": "path"}
+        }
+      }
+    },
+    {
+      "type": "symbol",
+      "from": {"data": "tree"},
+      "encode": {
+        "enter": {
+          "fill": {"scale": "color", "field": "id"},
+          "stroke": {"value": "white"},
+          "size": {"value": 400}
+        },
+        "update": {
+          "x": {"field": "x"},
+          "y": {"field": "y"}
+        }
+      }
+    }
+  ]
+}##";
+
+let chart: Vegalite = serde_json::from_str(spec)?;
     Ok(chart)
 }
