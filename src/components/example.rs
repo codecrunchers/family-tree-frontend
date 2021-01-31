3654bd8ed25f50286acba444523c88f7c3cb15f6
Alan Ryan <alan@codecrunchers.ie>
2021-01-31 22:59:43 +0000

    Increase stack size for graph

--- src/components/example.rs
+++ src/components/example.rs
@@ -24,43 +24,125 @@ pub fn call_vega() {
 }
 
 pub fn gen_chart() -> Result<Vegalite, Box<dyn std::error::Error>> {
-    // the chart
-    let chart = VegaliteBuilder::default()
-        .title("Choropleth of Unemployment Rate per County")
-        .data(
-            UrlDataBuilder::default()
-                .url("https://raw.githubusercontent.com/vega/vega-datasets/master/data/us-10m.json")
-                .format(
-                    DataFormatBuilder::default()
-                        .data_format_type(DataFormatType::Topojson)
-                        .feature("counties")
-                        .build()?,
-                )
-                .build()?,
-        )
-        .mark(Mark::Geoshape)
-        /*
-        .transform(vec![TransformBuilder::default()
-            .lookup("id")
-            .from(LookupDataBuilder::default()
-                .data(DataBuilder::default()
-                    .url("https://raw.githubusercontent.com/vega/vega-datasets/master/data/unemployment.tsv")
-                    .build()?)
-                .key("id")
-                .fields(vec!["rate".to_string()])
-                .build()?)
-            .build()?])
-        .projection(ProjectionBuilder::default().projection_type(ProjectionType::AlbersUsa).build()?)
-        .encoding(
-            EncodingBuilder::default()
-                .color(
-                    DefWithConditionMarkPropFieldDefStringNullBuilder::default()
-                        .field("rate")
-                        .def_with_condition_mark_prop_field_def_string_null_type(StandardType::Quantitative)
-                        .build()?,
-                )
-                .build()?,
-        )*/
-        .build()?;
+    let spec = r##"{
+  "$schema": "https://vega.github.io/schema/vega/v5.json",
+  "description": "An example of Cartesian layouts for a node-link diagram of hierarchical data.",
+  "width": 600,
+  "height": 1600,
+  "padding": 5,
+
+  "signals": [
+    {
+      "name": "labels", "value": true,
+      "bind": {"input": "checkbox"}
+    },
+    {
+      "name": "layout", "value": "tidy",
+      "bind": {"input": "radio", "options": ["tidy", "cluster"]}
+    },
+    {
+      "name": "links", "value": "diagonal",
+      "bind": {
+        "input": "select",
+        "options": ["line", "curve", "diagonal", "orthogonal"]
+      }
+    },
+    {
+      "name": "separation", "value": false,
+      "bind": {"input": "checkbox"}
+    }
+  ],
+
+  "data": [
+    {
+      "name": "tree",
+      "url": "data/flare.json",
+      "transform": [
+        {
+          "type": "stratify",
+          "key": "id",
+          "parentKey": "parent"
+        },
+        {
+          "type": "tree",
+          "method": {"signal": "layout"},
+          "size": [{"signal": "height"}, {"signal": "width - 100"}],
+          "separation": {"signal": "separation"},
+          "as": ["y", "x", "depth", "children"]
+        }
+      ]
+    },
+    {
+      "name": "links",
+      "source": "tree",
+      "transform": [
+        { "type": "treelinks" },
+        {
+          "type": "linkpath",
+          "orient": "horizontal",
+          "shape": {"signal": "links"}
+        }
+      ]
+    }
+  ],
+
+  "scales": [
+    {
+      "name": "color",
+      "type": "linear",
+      "range": {"scheme": "magma"},
+      "domain": {"data": "tree", "field": "depth"},
+      "zero": true
+    }
+  ],
+
+  "marks": [
+    {
+      "type": "path",
+      "from": {"data": "links"},
+      "encode": {
+        "update": {
+          "path": {"field": "path"},
+          "stroke": {"value": "#ccc"}
+        }
+      }
+    },
+    {
+      "type": "symbol",
+      "from": {"data": "tree"},
+      "encode": {
+        "enter": {
+          "size": {"value": 100},
+          "stroke": {"value": "#fff"}
+        },
+        "update": {
+          "x": {"field": "x"},
+          "y": {"field": "y"},
+          "fill": {"scale": "color", "field": "depth"}
+        }
+      }
+    },
+    {
+      "type": "text",
+      "from": {"data": "tree"},
+      "encode": {
+        "enter": {
+          "text": {"field": "name"},
+          "fontSize": {"value": 9},
+          "baseline": {"value": "middle"}
+        },
+        "update": {
+          "x": {"field": "x"},
+          "y": {"field": "y"},
+          "dx": {"signal": "datum.children ? -7 : 7"},
+          "align": {"signal": "datum.children ? 'right' : 'left'"},
+          "opacity": {"signal": "labels ? 1 : 0"}
+        }
+      }
+    }
+  ]
+}
+"##;
+    let chart: Vegalite = serde_json::from_str(spec)?;
     Ok(chart)
 }
